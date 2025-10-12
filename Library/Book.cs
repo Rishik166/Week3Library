8c8a0a4ff06442ae1a48ddbe8a435053ece809df
Rishik166 <rawat-r1@ulster.ac.uk>
2025-10-12 21:04:15 +0100

    Added a constructor the Book class.

--- Library/Book.cs
+++ Library/Book.cs
@@ -1,26 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.Runtime.CompilerServices;
 
-namespace Library
-{
-    public class Book
+public class Book
     {
 
-         public string Title;
-         public string Author;
-         public string ISBN;
+          string Title;
+          string Author;
+          string ISBN;
 
-        public void DisplayInfo()
-        {
-            Console.WriteLine($"Book title: {Title}");
-            Console.WriteLine($"Book Author: {Author}");
-            Console.WriteLine($"Book ISBN:{ISBN}");
-            Console.WriteLine();
+        //Example of a constructor that allows us to 'construct' a new
+        //Book object
+
+    public Book(string bookTitle, string bookAuthor,string bookISBN)
+    {
+        Title = bookTitle;
+        Author = bookAuthor;
+        ISBN = bookISBN;
+    }
+
+    void DisplayInfo()
+    {
+        Console.WriteLine($"Book title: {Title}");
+        Console.WriteLine($"Book Author: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}");
+        Console.WriteLine();
+    }
 
 
         }
-    }
-}
+    
+

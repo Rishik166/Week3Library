2e51e0302d6e731f533c88c983b36510f5327d19
Rishik166 <rawat-r1@ulster.ac.uk>
2025-10-12 20:52:54 +0100

    modified code

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using Library;
+
+Book book = new Book();
+// This information is for one book in our Library
+book.Title = "C# for beginners";
+book.Author = "BillGates";
+book.ISBN = "12345678";
+
+book.DisplayInfo();
+
+// This is for another book in our Library
+Book book1 = new Book();
+book1.Title = " C# Methods and classes";
+book1.Author = "Microsoft";
+book1.ISBN = "55667778";
+
+book1.DisplayInfo();
